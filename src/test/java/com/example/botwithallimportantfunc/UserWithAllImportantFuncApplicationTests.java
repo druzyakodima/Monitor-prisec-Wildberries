cb274446c10d7572a7612aad30bcf1a8994a88db
Dima <dimkadruz@icloud.com>
2022-11-13 19:25:54 +0500

    monitors

---
+++ src/test/java/com/example/botwithallimportantfunc/UserWithAllImportantFuncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botwithallimportantfunc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserWithAllImportantFuncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
