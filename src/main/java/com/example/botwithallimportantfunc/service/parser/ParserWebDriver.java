58b8befc66d189dde6c76b5766badb8aa7e33526
Dima <dimkadruz@icloud.com>
2022-11-14 20:08:24 +0500

    add ChromeOptions for heroku

--- src/main/java/com/example/botwithallimportantfunc/service/parser/ParserWebDriver.java
+++ src/main/java/com/example/botwithallimportantfunc/service/parser/ParserWebDriver.java
@@ -54,7 +54,7 @@ public class ParserWebDriver implements IParser {
     }
 
     public void getData(String url) {
-        webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(600));
+        webDriver.manage().timeouts().implicitlyWait(Duration.ofMillis(100));
         webDriver.get(url);
 
         data();
